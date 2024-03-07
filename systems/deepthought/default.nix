7d7ce06d1b878595d1fa70701e7fdb8eb2454f22
Brian Hechinger <wonko@4amlunch.net>
2024-03-07 08:54:50 +0000

    separated out networking

--- systems/deepthought/default.nix
+++ systems/deepthought/default.nix
@@ -11,6 +11,7 @@
       ../hardware/deepthought.nix
       ./development.nix
       ./grafana.nix
+      ./networking.nix
 #      ../common/nix-alien.nix
     ];
 
@@ -94,42 +95,6 @@
     (commonAutoMountOptions // { where = "/mnt/NetShare"; })
     (commonAutoMountOptions // { where = "/mnt/homes"; })
   ];
-
-  networking = {
-    hostName = "deepthought";
-    hostId = "39aa81bf";
-    domain = "4amlunch.net";
-    useDHCP = false;
-    bridges = {
-      "internal" = {
-        interfaces = [ "vlan420" ];
-      };
-      #"external" = {
-      #  interfaces = [ "vlan100" ];
-      #};
-    };
-    vlans = {
-      vlan420 = { id=420; interface="enp10s0"; };
-      #vlan100 = { id=100; interface="enp10s0"; };
-    };
-    interfaces = {
-      #external.useDHCP = true;
-      internal.ipv4.addresses = [{
-        address = "10.42.0.10";
-        prefixLength = 24;
-      }];
-      enp5s0.ipv4.addresses = [{
-        address = "192.168.99.10";
-        prefixLength = 24;
-      }];
-    };
-    defaultGateway = "10.42.0.1";
-    nameservers = [ "10.42.0.1" ];
-    extraHosts = ''
-      192.168.99.30 basket.4amlunch.net basket
-    '';
-    firewall.enable = false;
-  };
 
   time.timeZone = "Europe/Lisbon";
 
