f18a9d9c1ec6a895edbadcb6a9da4cf351818370
Brian Hechinger <wonko@4amlunch.net>
2024-05-24 14:31:13 +0100

    put users in their own file

---
+++ systems/deepthought/users.nix
@@ -0,0 +1,45 @@
+{ inputs, config, lib, pkgs, pkgs-brian, smc, npe, ... }:
+
+{
+  security.pam.loginLimits = [
+    { domain = "wonko"; item = "nofile"; type = "hard"; value = "524288"; }
+    { domain = "wayland"; item = "nofile"; type = "hard"; value = "524288"; }
+  ];
+
+  users.users = {
+    wonko = {
+      isNormalUser = true;
+      description = "Brian Hechinger";
+      shell = pkgs.zsh;
+      extraGroups = [
+        "wheel"
+        "audio"
+        "libvirtd"
+        "users"
+        "docker"
+        "kvm"
+        "wireshark"
+        "onepassword"
+        "onepassword-cli"
+        "qemu-libvirtd"
+      ];
+    };
+    wayland = {
+      isNormalUser = true;
+      description = "Wayland Testuser";
+      shell = pkgs.zsh;
+      extraGroups = [
+        "wheel"
+        "audio"
+        "libvirtd"
+        "users"
+        "docker"
+        "kvm"
+        "wireshark"
+        "onepassword"
+        "onepassword-cli"
+        "qemu-libvirtd"
+      ];
+    };
+  };
+}
