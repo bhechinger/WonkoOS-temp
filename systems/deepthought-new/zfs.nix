ef45cfcbb6c4e482caba0ac173f7668806592f39
Brian Hechinger <wonko@4amlunch.net>
2025-10-09 17:34:50 +0100

    added disko config

---
+++ systems/deepthought-new/zfs.nix
@@ -0,0 +1,63 @@
+{
+  disko.devices = {
+    disk = {
+      os = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        # Workaround: cannot import 'zroot': I/O error in disko tests
+        options.cachefile = "none";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+        };
+        mountpoint = "/";
+        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+
+        datasets = {
+          root = {
+            type = "zfs_fs";
+            mountpoint = "/";
+          };
+          var = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+          };
+          nix = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+          };
+          home = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+          };
+        };
+      };
+    };
+  };
+}
