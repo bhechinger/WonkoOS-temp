de127eda67585c1cce213cae12b7f2b22e7fc163
Brian Hechinger <wonko@4amlunch.net>
2025-10-15 17:47:28 +0100

    restored network config

--- systems/deepthought/networking.nix
+++ systems/deepthought/networking.nix
@@ -5,42 +5,34 @@
     hostName = "deepthought-new";
     hostId = "7ccf5c69";
     domain = "4amlunch.net";
-    #useDHCP = false;
-    #bridges = {
-    #  "trunk" = {
-    #    interfaces = [ "enp10s0" ];
-    #  };
-    #  "internal" = {
-    #    interfaces = [ "vlan420" ];
-    #  };
-    #  #"external" = {
-    #  #  interfaces = [ "vlan100" ];
-    #  #};
-    #};
-    #vlans = {
-    #  vlan420 = { id=420; interface="enp10s0"; };
-    #  #vlan100 = { id=100; interface="enp10s0"; };
-    #};
-    #interfaces = {
-    #  #external.useDHCP = true;
-    #  internal.ipv4.addresses = [{
-    #    address = "10.42.0.10";
-    #    prefixLength = 24;
-    #  }];
-    #  enp5s0.ipv4.addresses = [{
-    #    address = "192.168.99.10";
-    #    prefixLength = 24;
-    #  }];
-    #};
-    #defaultGateway = "10.42.0.1";
-    #nameservers = [ "10.42.0.2 10.42.0.12" ];
-    #192.168.99.30 basket.4amlunch.net basket
+    useDHCP = false;
+    bridges = {
+      "trunk" = {
+        interfaces = [ "enp5s0" ];
+      };
+      "storage" = {
+        interfaces = [ "enp6s0" ];
+      };
+    };
+    vlans = {
+      internal = { id=420; interface="trunk"; };
+    };
+    interfaces = {
+      internal.ipv4.addresses = [{
+        address = "10.42.0.229";
+        prefixLength = 24;
+      }];
+      storage.ipv4.addresses = [{
+        address = "192.168.99.229";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = "10.42.0.1";
+    nameservers = [ "10.42.0.2 10.42.0.12" ];
     extraHosts = ''
-      10.42.0.30 basket.4amlunch.net basket
-      192.168.49.2 bahmni.k8s payments-bahmni.k8s
+      192.168.99.30 basket.4amlunch.net basket
     '';
     firewall.enable = false;
-    useDHCP = lib.mkDefault true;
   };
 
   services.openssh.enable = true;
